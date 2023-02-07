3834f3e9ee8552d5071575ca630c0a97b04d5033
Erdem Ersoy <erdemersoy256@gmail.com>
2023-02-07 13:12:36 +0300

    Update example
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

--- examples/example.c
+++ examples/example.c
@@ -15,11 +15,12 @@ int main(void)
     print("Enter a floating-point number: ");
     float z = getFloat();
 
-    print("Max number: ");
+    print("Max number between the integers: ");
     printlnInt(max(x, y));
 
-    print("Min number: ");
+    print("Min number between the integers: ");
     printlnInt(min(x, y));
 
+    print("Absolute value of the floating-point number: ");
     printlnFloat(absValue(z));
 }
