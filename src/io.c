793f91a774d0c263e6816c6aed884812ec72f8ed
Erdem Ersoy <erdemersoy256@gmail.com>
2023-02-07 13:28:53 +0300

    Update formatting
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

--- src/io.c
+++ src/io.c
@@ -1,51 +1,62 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void print(char * text) {
+void print(char * text)
+{
     printf("%s", text);
 }
 
-void println(char * text) {
+void println(char * text)
+{
     printf("%s\n", text);
 }
 
-void printDouble(double value) {
+void printDouble(double value)
+{
     printf("%lf", value);
 }
 
-void printlnDouble(double value) {
+void printlnDouble(double value)
+{
     printf("%lf\n", value);
 }
 
-void printFloat(float value) {
+void printFloat(float value)
+{
     printf("%f", value);
 }
 
-void printlnFloat(float value) {
+void printlnFloat(float value)
+{
     printf("%f\n", value);
 }
 
-void printInt(int value) {
+void printInt(int value)
+{
     printf("%d", value);
 }
 
-void printlnInt(int value) {
+void printlnInt(int value)
+{
     printf("%d\n", value);
 }
 
-float getDouble() {
+float getDouble()
+{
     double number;
     scanf(" %lf", &number);
     return number;
 }
 
-float getFloat() {
+float getFloat()
+{
     float number;
     scanf(" %f", &number);
     return number;
 }
 
-int getInt() {
+int getInt()
+{
     int number;
     scanf(" %d", &number);
     return number;
