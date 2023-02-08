65dc9fa443bad61e002c2cb19117affea7d04ba9
Erdem Ersoy <erdemersoy256@gmail.com>
2023-02-08 12:13:09 +0300

    Add more string functions
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

--- include/strings.h
+++ include/strings.h
@@ -1,11 +1,19 @@
 #ifndef STRINGS_H
 #define STRINGS_H
 
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MAX_STR_LEN 256
 
+int stringCompare(char * str1, char * str2);
+int stringContains(char * str, char * substr);
+int stringLength(char * str);
+
+char * concatenatedString(char * str1, char * str2);
+char * loweredString(char * str);
+char * upperedString(char * str);
 char * reversedString(char * str);
 
 #endif
--- src/strings.c
+++ src/strings.c
@@ -1,8 +1,72 @@
 #include "../include/strings.h"
 
+int stringCompare(char * str1, char * str2)
+{
+    return strcmp(str1, str2);
+}
+
+int stringContains(char * str, char * substr)
+{
+    char * occurrence = strstr(str, substr);
+
+    if (occurrence == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int stringLength(char * str)
+{
+    return strlen(str);
+}
+
+char * concatenatedString(char * str1, char * str2)
+{
+    int lenstr1 = strlen(str1);
+    int lenstr2 = strlen(str2);
+    char * result = (char *)calloc(1, lenstr1 + lenstr2 + 1);
+    strcpy(result, str1);
+    strcat(result, str2);
+    return result;
+}
+
+char * loweredString(char * str)
+{
+    int lenstr = stringLength(str);
+    char * result = (char *)calloc(1, lenstr + 1);
+
+    for (int i = 0; i < lenstr; i++)
+    {
+        result[i] = tolower(str[i]);
+    }
+
+    result[lenstr] = '\0';
+
+    return result;
+}
+
+char * upperedString(char * str)
+{
+    int lenstr = stringLength(str);
+    char * result = (char *)calloc(1, lenstr + 1);
+
+    for (int i = 0; i < lenstr; i++)
+    {
+        result[i] = toupper(str[i]);
+    }
+
+    result[lenstr] = '\0';
+
+    return result;
+}
+
 char * reversedString(char * str)
 {
-    size_t lenstr = strlen(str);
+    size_t lenstr = stringLength(str);
     char * reversedString = calloc(1, sizeof(char) * (lenstr + 1));
 
     for (int i = 0; i < (int)lenstr; i++)
