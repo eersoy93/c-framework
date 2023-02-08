3be9ff65077d77665f06fe49f006d2448699546d
Erdem Ersoy <erdemersoy256@gmail.com>
2023-02-08 11:00:00 +0300

    Add array sorting functions
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

--- src/memory.c
+++ src/memory.c
@@ -25,3 +25,81 @@ void swap_int(int * a, int * b)
     *a = *b;
     *b = temp;
 }
+
+void sort_double(double arr[], int n, _Bool ascending)
+{
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (ascending)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    swap_double(&arr[j], &arr[j + 1]);
+                }
+            }
+            else
+            {
+                if (arr[j] < arr[j + 1])
+                {
+                    swap_double(&arr[j], &arr[j + 1]);
+                }
+            }
+        }
+    }
+}
+
+void sort_float(float arr[], int n, _Bool ascending)
+{
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (ascending)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    swap_float(&arr[j], &arr[j + 1]);
+                }
+            }
+            else
+            {
+                if (arr[j] < arr[j + 1])
+                {
+                    swap_float(&arr[j], &arr[j + 1]);
+                }
+            }
+        }
+    }
+}
+
+void sort_int(int arr[], int n, _Bool ascending)
+{
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (ascending)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    swap_int(&arr[j], &arr[j + 1]);
+                }
+            }
+            else
+            {
+                if (arr[j] < arr[j + 1])
+                {
+                    swap_int(&arr[j], &arr[j + 1]);
+                }
+            }
+        }
+    }
+}
