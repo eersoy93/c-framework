10567a411293fdd34412949a4005628ff3f3ed92
Erdem Ersoy <erdemersoy256@gmail.com>
2023-02-09 16:39:05 +0300

    Fix errors
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

--- include/types.h
+++ include/types.h
@@ -4,21 +4,21 @@
 
 typedef struct tagFloatingPoint
 {
-    double value = 0.0;
+    double value;
 } FloatingPointType;
 
 typedef struct tagInteger
 {
-    int value = 0;
+    int value;
 } IntegerType;
 
 typedef struct tagSizeInteger
 {
-    size_t value = 0;
+    size_t value;
 } SizeIntegerType;
 
 typedef struct tagString
 {
-    wint_t * value = L"\0";
-    SizeIntegerType length = 0;
+    wint_t * value;
+    SizeIntegerType length;
 } StringType;
